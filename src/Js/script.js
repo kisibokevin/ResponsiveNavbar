9d7a2e9866453f8754ce5c0471687e8a227f2bc5
kisibokevin <kisibokevin@gmail.com>
2023-12-28 18:21:24 +0300

    changes to src folder and after click of link to close menu bar

--- src/Js/script.js
+++ src/Js/script.js
@@ -1,7 +1,15 @@
 
 const toggleButton = document.getElementsByClassName('toggle-btn')[0];
 const navbarLinks = document.getElementsByClassName('links')[0];
+const closeMenu = document.querySelectorAll('.nav-link');
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+
+closeMenu.forEach(link => {
+    link.addEventListener('click', () => {
+    navbarLinks.classList.remove('active');
+  })
+});
+
